e2ce7859a9e5d03c1a29b5d8f31b33657e1a6b55
Aditya <apple@Adityas-MacBook-Air.local>
2026-05-07 21:35:35 +0530

    Added Patterns-1

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -8,3 +8,14 @@ public class Pattern1 {
         }
     }
 }
+
+public class Pattern1_2 {
+    public static void main(String[] args){
+        for( int i =0;i<4;i++){
+            for( int j = 0;j<=i;j++){
+                System.out.print("0");
+            }
+            System.out.println();
+        }
+    }
+}
