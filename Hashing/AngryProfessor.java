ccce5d82cc5b3b8715a8674bc218968bf5cbc6ed
Aditya <apple@Adityas-MacBook-Air.local>
2026-05-20 11:55:19 +0530

    Solved

---
+++ Hashing/AngryProfessor.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0){
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i= 0;i<n;i++){
+                arr[i] = sc.nextInt();
+            }
+            int count = 0;
+            for(int i =0;i<n;i++){
+                if(arr[i] <= 0){
+                    count++;
+                }
+            }
+            if(count >= k){
+                System.out.println("NO");
+            }else {
+                System.out.println("YES");
+            }
+            t--;
+        }
+    }
+}
