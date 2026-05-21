8998897d94728da37aac183bc409fa94e7bf533f
Aditya <apple@Adityas-MacBook-Air.local>
2026-05-21 15:40:16 +0530

    Solved

---
+++ Contest/DetermineConflicts.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean haveConflict(String[] event1, String[] event2) {
+        String start1 = event1[0];
+        String end1 = event1[1];
+        String start2 = event2[0];
+        String end2 = event2[1];
+        if(end1.compareTo(start2) < 0 || end2.compareTo(start1) < 0){
+            return false;
+        }
+        return true;
+    }
+}
