b0d352b7077fa9def835c90160d24b9d34fdbf42
Aditya <apple@Adityas-MacBook-Air.local>
2026-05-29 18:41:45 +0530

    Solved

---
+++ Hashing/4SumII.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int ans = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums3.length;i++){
+            for(int j = 0;j<nums4.length;j++){
+                int sum = nums3[i] + nums4[j];
+                map.put(sum,map.getOrDefault(sum,0)+1);
+            }
+        }
+        for(int i = 0;i<nums1.length;i++){
+            for(int j = 0;j<nums2.length;j++){
+                int target = -(nums1[i] + nums2[j]);//a+b+c+d = 0 and now c+d = -(a+b)
+                if(map.containsKey(target)){
+                    ans += map.get(target);
+                }
+            }
+        }
+        return ans;
+    }
+}
